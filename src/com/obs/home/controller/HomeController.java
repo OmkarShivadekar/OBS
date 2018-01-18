a8be3a5fe5820b1e102623b4bd72bf23c6dfee87
sarang <sarang@sarang-Inspiron-3521>
2018-01-18 15:09:30 +0530

    build SRS

---
+++ src/com/obs/home/controller/HomeController.java
@@ -0,0 +1,16 @@
+/**
+ * 	  Author : SARANG KAMBLE
+ * 	Document : HomeController.java
+ *		Date : 17-Jan-2018
+ * 		Time : 9:57:11 PM
+ */
+package com.obs.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/HOME")
+public class HomeController {
+
+}
---
+++ src/com/obs/home/dao/HomeDAOImpl.java
@@ -0,0 +1,14 @@
+/**
+ * 	  Author : SARANG KAMBLE
+ * 	Document : HomeDAOImpl.java
+ *		Date : 17-Jan-2018
+ * 		Time : 9:59:06 PM
+ */
+package com.obs.home.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HomeDAOImpl implements HomeDAO {
+
+}
