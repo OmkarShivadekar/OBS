9c4e14beaf227b60545ea7964d46d477a3287023
sarang <sarang@sarang-Inspiron-3521>
2018-01-29 11:26:10 +0530

    home page completed...

--- src/com/obs/home/service/HomeService.java
+++ src/com/obs/home/service/HomeService.java
@@ -1,17 +1,19 @@
 /**
  * 	  Author : SARANG KAMBLE
  * 	Document : HomeService.java
- *		Date : 17-Jan-2018
- * 		Time : 10:00:39 PM
+ *		Date : 25-Jan-2018
+ * 		Time : 11:11:46 AM
  */
 package com.obs.home.service;
 
 import java.util.List;
 
-import com.obs.entity.Home;
+import com.obs.entity.AddPost;
 
 public interface HomeService {
+	
+	public List<AddPost> getBooks();
 
-	List<Home> getBookDetails();
+	public AddPost getBook(int theId);
 
 }
--- src/com/obs/home/service/HomeServiceImpl.java
+++ src/com/obs/home/service/HomeServiceImpl.java
@@ -1,31 +1,38 @@
 /**
  * 	  Author : SARANG KAMBLE
  * 	Document : HomeServiceImpl.java
- *		Date : 17-Jan-2018
- * 		Time : 10:01:14 PM
+ *		Date : 25-Jan-2018
+ * 		Time : 11:13:34 AM
  */
 package com.obs.home.service;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.obs.entity.Home;
+import com.obs.entity.AddPost;
 import com.obs.home.dao.HomeDAO;
 
 @Service
 public class HomeServiceImpl implements HomeService {
 
-	private HomeDAO homeDAO; 	
+	@Autowired
+	private HomeDAO homeDAO;
 	
 	@Override
 	@Transactional
-	public List<Home> getBookDetails() {
-
-		return homeDAO.getBookDetails();
+	public List<AddPost> getBooks() {
+		
+		return homeDAO.getBooks();
 	}
 
-	
+	@Override
+	@Transactional
+	public AddPost getBook(int theId) {
+		
+		return homeDAO.getBook(theId);
+	}
+
 }
